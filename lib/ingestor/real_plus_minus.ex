8cd7c9f2dbd7574bc062270c96912e871305ee30
William Carroll <wcarroll@wustl.edu>
2017-05-09 23:51:58 -0400

    feat(real_plus_minus): Creates structs for RPM across diff positions

--- lib/ingestor/real_plus_minus.ex
+++ lib/ingestor/real_plus_minus.ex
@@ -2,8 +2,11 @@ defmodule Sandbox.Ingestor.RealPlusMinus do
   @moduledoc false
 
   alias Sandbox.DepthChart
+  alias __MODULE__
 
-  @source_template "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/"
+  @local_path "rpm_"
+  @remote_path "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/"
+
   @position_to_page_num %{
     point_guard:    1,
     shooting_guard: 2,
@@ -12,15 +15,84 @@ defmodule Sandbox.Ingestor.RealPlusMinus do
     center:         9,
   }
 
+  defstruct [
+    :ranking,
+    :player_name,
+    :team,
+    :games_played,
+    :minutes_per_game,
+    :offensive_rpm,
+    :defensive_rpm,
+    :rpm,
+    :wins,
+  ]
 
-  def read do
+
+  def rpm_for_position(position) do
+    page_for_position(position)
+    |> File.read!()
+    |> Floki.parse()
+    |> Floki.find(".mod-content table tr")
+    |> Stream.drop(1) # drop the headers
+    |> Stream.map(&decode_row/1) # drop the headers
+    |> Enum.into([])
   end
 
-  @spec page_for_position(DepthChart.position) :: pos_integer
-  def page_for_position(position) do
-    page =
+
+  @doc """
+  Decodes an HTML row into a `RealPlusMinus.t` struct.
+
+  """
+  def decode_row({"tr", _attrs, children}) do
+    [rk, name, team, gp, mpg, orpm, drpm, rpm, wins] =
+      children
+
+    %RealPlusMinus{
+      ranking: rk |> Floki.text() |> String.to_integer(),
+      player_name: name |> Floki.text() |> String.trim(),
+      team: team |> Floki.text() |> String.trim(),
+      games_played: gp |> Floki.text() |> String.to_integer(),
+      minutes_per_game: mpg |> Floki.text() |> String.to_float(),
+      offensive_rpm: orpm |> Floki.text() |> String.to_float(),
+      defensive_rpm: drpm |> Floki.text() |> String.to_float(),
+      rpm: rpm |> Floki.text() |> String.to_float(),
+      wins: wins |> Floki.text() |> String.to_float(),
+    }
+  end
+
+
+  @doc """
+  Returns the URI for RPM data for a specific position.
+
+  ## Positions
+
+    - `:point_guard`
+    - `:shooting_guard`
+    - `:small_forward`
+    - `:power_forward`
+    - `:center`
+
+  ## Options
+
+    - `:remote`: If set to `true`, the page returned will be the URI for the
+    remote source. Defaults to `false`.
+
+  """
+  @spec page_for_position(DepthChart.position, opts) :: pos_integer when
+  opts: [{:remote, boolean}]
+  def page_for_position(position, opts \\ []) do
+    remote? =
+      opts[:remote] || false
+
+    base =
+      case remote? do
+        true  -> @remote_path
+        false -> @local_path
+      end
+
+    page_num =
       Map.get(@position_to_page_num, position)
 
-    Path.join(@source_template, inspect(page))
+    base <> inspect(page_num)
   end
 end
--- test/ingestor/real_plus_minus_test.exs
+++ test/ingestor/real_plus_minus_test.exs
@@ -4,29 +4,59 @@ defmodule Sandbox.Ingestor.RealPlusMinusTest do
   alias Sandbox.Ingestor.RealPlusMinus
 
   describe "page_for_position/1" do
-    test "works for point guards" do
+    test "(local) works for point guards" do
       assert RealPlusMinus.page_for_position(:point_guard) ==
-        "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/1"
+        "rpm_1"
     end
 
-    test "works for shooting guards" do
-      assert RealPlusMinus.page_for_position(:shooting_guard) ==
+    test "(remote) works for shooting guards" do
+      assert RealPlusMinus.page_for_position(:shooting_guard, remote: true) ==
         "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/2"
     end
 
-    test "works for small forwards" do
+    test "(local) works for small forwards" do
       assert RealPlusMinus.page_for_position(:small_forward) ==
-        "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/5"
+        "rpm_5"
     end
 
-    test "works for power forwards" do
-      assert RealPlusMinus.page_for_position(:power_forward) ==
+    test "(remote) works for power forwards" do
+      assert RealPlusMinus.page_for_position(:power_forward, remote: true) ==
         "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/6"
     end
 
-    test "works for center" do
+    test "(local) works for center" do
       assert RealPlusMinus.page_for_position(:center) ==
-        "https://www.espn.com/nba/statistics/rpm/_/sort/RPM/position/9"
+        "rpm_9"
+    end
+  end
+
+  describe "decode_row/1" do
+    test "works" do
+      pg =
+        {"tr", [{"class", "oddrow"}],
+         [{"td", [], ["1"]},
+          {"td", [],
+           [{"a", [{"href", "https://www.espn.com/nba/player/_/id/2779/chris-paul"}],
+             ["Chris Paul"]}]}, {"td", [], ["LAC"]},
+          {"td", [{"style", "text-align:right;"}], ["61"]},
+          {"td", [{"style", "text-align:right;"}], ["31.5"]},
+          {"td", [{"style", "text-align:right;"}], ["5.16"]},
+          {"td", [{"style", "text-align:right;"}], ["2.77"]},
+          {"td", [{"style", "text-align:right;"}, {"class", "sortcell"}], ["7.93"]},
+          {"td", [{"style", "text-align:right;"}], ["13.50"]}]}
+
+      assert RealPlusMinus.decode_row(pg) ==
+        %RealPlusMinus{
+          ranking: 1,
+          player_name: "Chris Paul",
+          team: "LAC",
+          games_played: 61,
+          minutes_per_game: 31.5,
+          offensive_rpm: 5.16,
+          defensive_rpm: 2.77,
+          rpm: 7.93,
+          wins: 13.50,
+        }
     end
   end
 end
